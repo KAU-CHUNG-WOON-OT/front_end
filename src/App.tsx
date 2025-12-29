50bbd49f3af26d3f2d4a93bdca170cb70954f6d2
이상원 <sangwon020214@gmail.com>
2025-12-29 14:39:14 +0900

    feat: 학교 엠블럼 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import emblemImg from "./assets/emblem.svg";
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -13,13 +14,13 @@ function App() {
       <Header onOpenSidebar={openSidebar} />
       <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
 
-      <main className="flex-1 flex justify-center items-center text-center p-8">
-        <h1 className="text-4xl font-bold leading-snug">
-          2025<br />
-          새내기배움터<br />
-          로고 or 학교<br />
-          마크
-        </h1>
+      <main className="flex-1 flex flex-col justify-center items-center text-center p-8 gap-6">
+        
+        <img 
+          src={emblemImg} 
+          alt="학교 엠블럼" 
+          className="w-64 h-auto object-contain drop-shadow-md" 
+        />
       </main>
     </div>
   );
