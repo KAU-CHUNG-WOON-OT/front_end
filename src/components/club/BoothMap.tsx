9a2209cc0a81ed13098d09df7358d85bc38bf206
이상원 <sangwon020214@gmail.com>
2026-02-23 17:21:37 +0900

    fix: 동아리 부스맵 사진 교체

--- src/components/club/BoothMap.tsx
+++ src/components/club/BoothMap.tsx
@@ -1,6 +1,6 @@
 import { useState, useRef } from "react";
 import booth1 from "../../assets/booth1.png";
-import booth2 from "../../assets/booth2.jpg";
+import booth2 from "../../assets/booth2.png";
 
 interface BoothMapProps {
   onClick: (imageSrc: string) => void;
